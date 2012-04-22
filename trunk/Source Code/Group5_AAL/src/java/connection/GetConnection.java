9c5d26936e0286801bce38d91b01268fa22e90d6
hieudq_b00385@fpt.aptech.ac.vn <hieudq_b00385@fpt.aptech.ac.vn@c11252cf-9afe-6bb2-deee-ef79ec0f118f>
2012-04-22 02:29:55 +0000

    Config lai toan bo project ..

---
+++ trunk/Source Code/Group5_AAL/src/java/connection/GetConnection.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Pe Yeu
+ */
+public class GetConnection {
+
+    Connection con = null;
+    private static ResourceBundle bundle = ResourceBundle.getBundle("Connection");
+    private String host;
+    private String pcName;
+    private String port;
+    private String databaseName;
+    private String user;
+    private String pass;
+
+    public Connection getConnec() {
+        try {
+            host = bundle.getString("host");
+            pcName = bundle.getString("pcName");
+            port = bundle.getString("port");
+            databaseName = bundle.getString("databaseName");
+            user = bundle.getString("user");
+            pass = bundle.getString("pass");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection("jdbc:sqlserver://" + host + "\\" + pcName + ":" + port + ";databaseName=" + databaseName + "", user, pass);
+            if (con != null) {
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GetConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(GetConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+}
